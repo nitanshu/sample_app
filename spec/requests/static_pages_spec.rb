3bea61ff5d1d06bcfd04acf69cb3dfa684e5ce0f
nitanshu <nitanshu1991@gmail.com>
2013-08-23 14:25:44 +0530

    my final commit in 4

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,10 +5,13 @@ it "should have the h1 'Sample App'" do
 visit '/static_pages/home'
 page.should have_selector('h1', :text => 'Sample App')
 end
-it "should have the title 'Home'" do
+it "should have the base title" do
 visit '/static_pages/home'
-page.should have_selector('title',
-:text => "Ruby on Rails Tutorial Sample App | Home")
+page.should have_selector('title', :text => "Nitanshu Sample App | Home")
+end
+it "should not have a custom page title" do
+visit '/static_pages/home'
+page.should have_selector('title', :text => '|HOME')
 end
 end
 describe "Help page" do
@@ -18,8 +21,7 @@ page.should have_selector('h1', :text => 'Help')
 end
 it "should have the title 'Help'" do
 visit '/static_pages/help'
-page.should have_selector('title',
-:text => "Ruby on Rails Tutorial Sample App | Help")
+page.should have_selector('title', :text => "#{base_title} | Help")
 end
 end
 describe "About page" do
@@ -29,8 +31,17 @@ page.should have_selector('h1', :text => 'About Us')
 end
 it "should have the title 'About Us'" do
 visit '/static_pages/about'
-page.should have_selector('title',
-:text => "Ruby on Rails Tutorial Sample App | About Us")
+page.should have_selector('title', :text => "#{base_title} | About Us")
+end
+end
+describe "Contact page" do
+it "should have the h1 'Contact'" do
+visit '/static_pages/contact'
+page.should have_selector('h1', :text => 'Contact')
+end
+it "should have the title 'Contact'" do
+visit '/static_pages/contact'
+page.should have_selector('title', :text => "#{base_title} | Contact")
 end
 end
 end
