65a9d22b6a04bbaf793426c17cea2c8df8eca439
nitanshu <nitanshu1991@gmail.com>
2013-08-30 14:01:11 +0530

    finish layout & routes

--- Gemfile
+++ Gemfile
@@ -1,11 +1,12 @@
 source 'https://rubygems.org'
 gem 'rails', '3.2.5'
+gem 'bootstrap-sass' 
 group :development, :test do
 gem 'sqlite3', '1.3.5'
 gem 'rspec-rails', '2.10.0'
 end
 # Gems used only for assets and not required
-# in production environments by default.
+# in production environments by defau
 group :assets do
 gem 'sass-rails', '3.2.4'
 gem 'coffee-rails', '3.2.2'
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new"
+match '/home', to: 'static_pages#home'
 
-  get "static_pages/help"
-  
-  get "static_pages/about"
-
-  get "static_pages/contact"
+match '/signup', to: 'users#new'
+match '/help', to: 'static_pages#help'
+match '/about', to: 'static_pages#about'
+match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -54,11 +54,11 @@ SampleApp::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
+  # You can have the root of your site routed with 
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
 
-  # See how all your routes lay out with "rake routes"
+  # See how all your routes lay out with 
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
