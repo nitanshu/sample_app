b37573adce5f2e1dea5e4baa507a99297dc73fb3
nitanshu <nitanshu1991@gmail.com>
2013-09-15 20:29:45 +0530

    finish user signup

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'uglifier', '1.2.3'
 end
 gem 'jquery-rails'
 group :test do
+gem 'factory_girl_rails'
 gem 'capybara', '1.1.2'
 end
 group :production do
---
+++ spec/factories.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+factory :user do
+name "Akash"
+email "abc@abc.com"
+password "abcabc"
+password_confirmation "abcabc"
+name "Nitanshu verma"
+email "nitanshu1991@gmail.com"
+password "Nit@shu71"
+password_confirmation "Nit@nshu71"
+end
+end
